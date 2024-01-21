dec3b94232f7698d2ea7423d1a7872546e44fd27
Wong Yen Chik <70868257+Wongyenchik@users.noreply.github.com>
2024-01-21 16:54:56 +0800

    Set base path for Github pages deployment

---
+++ vite.config.js
@@ -0,0 +1,12 @@
+// import vue from "@vitejs/plugin-vue";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+    // assetsInclude: ["**/*.gltf"],
+    base: "/arnav/",
+    // publicDir: "static/",
+    // server: {
+    //     host: true,
+    // },
+    // plugins: [vue()],
+});
